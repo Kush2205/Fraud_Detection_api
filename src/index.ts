1e6f834e619a093b9b44855c9bd52946e04d1ab5
Kush2205 <kr.kushagra2205@gmail.com>
2025-03-21 15:42:58 +0530

    api finished

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import {connectDB} from './db/index';
-
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.get('/fraud-apps', async (req, res) => {
     await connectDB();
     const fraudApps = await mongoose.connection.collection('FraudulentApp').find().toArray();
